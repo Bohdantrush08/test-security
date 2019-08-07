0a31a016a9a414589fc657c271b6b221045fc53b
BohdanMytnyk <bbmytnyk@gmail.com>
2019-08-07 18:15:49 +0300

    added UserRepository, UserService, getters/setters for User

--- src/main/java/com/turtles/testsecurity/repository/UserRepository.java
+++ src/main/java/com/turtles/testsecurity/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.turtles.testsecurity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User getOneByEmail(String email);
+    User getOneByEmail(String email);
 }
